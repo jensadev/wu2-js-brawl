b3eb1b7da9065383fd0c31323bd65b0321a15b9c
jensadev <jensandreasson77@gmail.com>
2025-11-03 10:52:10 +0100

    add combat log functionality and style to display game events

--- main.js
+++ main.js
@@ -9,20 +9,27 @@ function rollDice() {
 
 const playerHpElement = document.querySelector("#player-hp")
 const enemyHpElement = document.querySelector("#enemy-hp")
+const combatLogElement = document.querySelector("#combat-log")
+
+function log(msg) {
+    const li = document.createElement("li")
+    li.textContent = msg
+    combatLogElement.appendChild(li)
+}
 
 function gameRound() {
     const playerRoll = rollDice()
     const enemyRoll = rollDice()
     if (playerRoll > enemyRoll) {
         const damage = playerRoll - enemyRoll
-        console.log(`Du köttar fienden för ${damage}!`)
+        log(`Du köttar fienden för ${damage}!`)
         enemyHp -= damage
     } else if (enemyRoll > playerRoll) {
         const damage = enemyRoll - playerRoll
-        console.log(`Nedrans, du blir mulad för ${damage}!`)
+        log(`Nedrans, du blir mulad för ${damage}!`)
         playerHp -= damage
     } else {
-        console.log("Snyggt parerat, inget händer!")
+        log("Snyggt parerat, inget händer!")
     }
     playerHpElement.textContent = playerHp
     enemyHpElement.textContent = enemyHp
